96264b18a22780cbe3924c24090c8aeeb287d927
soroushzargar <soroushzargar@gmail.com>
2026-05-10 11:23:09 +0200

    other format of website

--- components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="py-8 text-sm text-center text-gray-500 dark:text-gray-400">
-      © {new Date().getFullYear()} Your Name. All rights reserved.
-    </footer>
-  )
-}
